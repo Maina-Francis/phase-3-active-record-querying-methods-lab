e49603ad84f33916ef8b1057e1a004845e5b5236
Francis Maina <sebastianfrancix@gmail.com>
2022-11-06 07:08:53 +0300

    Completed assignment

---
+++ db/seeds.rb
@@ -0,0 +1,4 @@
+Show.create(name: "Househelps of Kawangware", network: "NTV", day: "Saturday", rating: 6, season: "Four")
+Show.create(name: "Maria", network: "Citizen", day: "Friday", rating: 4, season: "One")
+Show.create(name: "Beba Beba", network: "NTV", day: "Saturday", rating: 7, season: "Two")
+Show.create(name: "Mother-In-Law", network: "Citizen", day: "Tuesday", rating: 3, season: "Five")
